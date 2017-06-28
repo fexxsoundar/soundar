8a5e7cb62ca03bb9f908da8e083b23813d86bbc0
fexxsoundar <sukumarsukuskr469@gmail.com>
2017-06-28 14:39:02 +0530

    Create soundar.c

---
+++ soundar.c
@@ -0,0 +1,20 @@
+void main()
+{
+int s;
+clrscr();
+printf("Enter the number to be Checked");
+scanf("%d",&s);
+if (s>0)
+{
+printf("\nThe number is a positive number");
+}
+else if(s<0)
+{
+printf("\n The number is a Negative Number");
+}
+else
+{
+printf("The number is zero");
+}
+getch();
+}
