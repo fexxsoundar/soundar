ded4abe230f1dfe12ac6952b1641a55d15969314
fexxsoundar <sukumarsukuskr469@gmail.com>
2017-06-29 14:38:58 +0530

    Rename vowel.c to vowel1.c

---
+++ vowel1.c
@@ -0,0 +1,16 @@
+void main()
+{
+char s;
+clrscr();
+printf("Enter the elment to be found");
+scanf("%s",&s);
+if((s=='a')||(s=='e')||(s=='i')||(s=='o')||(s=='u')||(s=='A')||(s=='E')||(s=='I')||(s=='O')||(s=='U'))
+{
+printf("the element is a vowel");
+}
+else
+{
+printf("The elenemt is not a vowel");
+}
+getch();
+}
